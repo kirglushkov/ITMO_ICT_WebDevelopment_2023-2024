064dde4de0fa72b0cd912dec6e9a0be9eab22ae9
Kirill Glushkov <kirglushkovv@gmail.com>
2023-10-30 12:37:48 +0300

    fix

--- students/k33420/Kirill_Glushkov/lr2/hotel_manage/hotel/admin.py
+++ students/k33420/Kirill_Glushkov/lr2/hotel_manage/hotel/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Hotel, Room, HotelForm, RoomForm
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    form = HotelForm
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    form = RoomForm
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if obj:
+            form.base_fields["hotel"].widget.can_add_related = False
+        return form
